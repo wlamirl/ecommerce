7b77ff73b2f290f91719d5b43fe48b3f4f9d20b7
wlamirl <wlamirl@yahoo.com.br>
2023-09-28 14:09:55 -0300

    First commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        // private readonly StoreContext context;
+    private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            // this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            // var products = context.Products.ToList();
+            return await _context.Products.ToListAsync();
+
+            // return Ok(products);
+        }
+
+        [HttpGet("{id}")] // api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            // return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
+        }
+
+    }
+}
